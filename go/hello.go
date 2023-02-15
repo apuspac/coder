fd37fca2383ffcb69b89ce4a27b1609b5a0c765e
apuspac <penginstar1299@gmail.com>
2023-02-15 11:06:39 +0900

    name change 0801->ABC

---
+++ go/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Greet() {
+	fmt.Println("hello!")
+}
+
+// ポインタ関係はほぼCと同じだと思います。
+
+func main() {
+	fmt.Println("hello, world")
+}
---
+++ python/Beginners-Selection/ABC81-A/main.py
@@ -0,0 +1,8 @@
+s = list(input())
+
+count = 0
+for i in s:
+    if i == "1":
+        count += 1
+
+print(count)
